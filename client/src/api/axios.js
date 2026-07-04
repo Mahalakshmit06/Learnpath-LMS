d6902eeec7eab171eef8627e3c03be29caa9a3a2
Mahalakshmit06 <mahalakshmit209@gmail.com>
2026-07-04 20:21:21 +0530

    Initial commit - LearnPath LMS

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://learnpath-lms-backend.onrender.com",
+  baseURL: "",
   headers: {
     "Content-Type": "application/json",
   },
--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,10 +1,14 @@
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 import AppRoutes from "../routes/AppRoutes";
+import ScrollToTop from "../components/common/ScrollToTop";
 
 const MainLayout = () => {
   return (
     <div className="min-h-screen bg-[#020617] text-white flex flex-col">
+      {/* Automatically scroll to top on every route change */}
+      <ScrollToTop />
+
       <Navbar />
 
       <main className="flex-1">
