51b1f483c785d9f82fc8356352c5b4d48e992caa
Mahalakshmit06 <mahalakshmit209@gmail.com>
2026-07-04 20:46:29 +0530

    Update API URL for production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "",
+  baseURL: "https://learnpath-lms-fj5o.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
